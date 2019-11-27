a36f10c14e81d34b9abb8616497d33e4cfa0e58a
wryang <yang@yang-mac.local>
2019-11-27 12:29:57 +0900

    재커밋

--- src/main/java/xyz/anythings/dps/DpsConstants.java
+++ src/main/java/xyz/anythings/dps/DpsConstants.java
@@ -9,5 +9,9 @@ import xyz.anythings.base.LogisConstants;
  */
 public class DpsConstants extends LogisConstants {		
 	
+	/**
+	 * 모듈의 기본 작업 타입 
+	 */
+	public static final String MODULE_DEFAULT_JOB_TYPE = LogisConstants.JOB_TYPE_DPS;
 	
 }
