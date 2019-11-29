fef61d79a2ff4304c4c50ee3d5d21faa7ebfdbaf
wryang <yang@yang-mac.local>
2019-11-29 21:34:24 +0900

    dps 설정 적용 및 병합 작업중...

--- src/main/java/xyz/anythings/dps/service/DpsInstructionService.java
+++ src/main/java/xyz/anythings/dps/service/DpsInstructionService.java
@@ -1,6 +1,6 @@
 package xyz.anythings.dps.service;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +12,10 @@ import xyz.anythings.base.event.EventConstants;
 import xyz.anythings.base.service.api.IInstructionService;
 import xyz.anythings.base.service.impl.AbstractInstructionService;
 import xyz.anythings.dps.service.util.DpsBatchJobConfigUtil;
+import xyz.anythings.dps.service.util.DpsStageJobConfigUtil;
 import xyz.anythings.sys.event.model.EventResultSet;
+import xyz.anythings.sys.util.AnyOrmUtil;
+import xyz.elidom.sys.util.DateUtil;
 import xyz.elidom.util.ValueUtil;
 
 /**
@@ -37,6 +40,7 @@ public class DpsInstructionService extends AbstractInstructionService implements
 	 * @params
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public int instructBatch(JobBatch batch, List<String> equipIdList, Object... params) {
 		// 1. batch 의 equip_type, group 에 따라 설비 list 를 가져온다 .
@@ -44,13 +48,24 @@ public class DpsInstructionService extends AbstractInstructionService implements
 		
 		int retCnt = 0;
 		
-		// 2. 대상 분류 
+		// 2. Rack 에 작업 할당 
+		List<Rack> rackList = (List<Rack>)equipList;
+		Date now = DateUtil.getDate();
+		for(Rack rack : rackList) {
+			rack.setUpdatedAt(now);
+			rack.setBatchId(batch.getId());
+		}
+		
+		AnyOrmUtil.updateBatch(rackList, 100, "updatedAt", "batchId");
+		
+
+		// 3. 대상 분류 
 		retCnt += this.instructBatchOrderType(batch, equipList, params);
 		
-		// 3. 작업 지시 
+		// 4. 작업 지시 
 		retCnt += this.instructBatchStart(batch, equipList, params);
 		
-		// 4. 작업 지시 후 박스 요청 
+		// 5. 작업 지시 후 박스 요청 
 		retCnt += this.instructBatchBoxReq(batch, equipList, params);
 		
 		return retCnt;
@@ -74,8 +89,21 @@ public class DpsInstructionService extends AbstractInstructionService implements
 
 	@Override
 	public int mergeBatch(JobBatch mainBatch, JobBatch newBatch, Object... params) {
-		// TODO Auto-generated method stub
-		return 0;
+		// 1. batch 의 equip_type, group 에 따라 설비 list 를 가져온다 .
+		List<?> equipList = this.getBatchEquipList(mainBatch, null);
+				
+		int retCnt = 0;
+
+		// 2. 대상 분류 
+		retCnt += this.instructBatchOrderType(newBatch, equipList, params);
+		
+		// 3. 작업 병합  
+		retCnt += this.mergeBatchStart(mainBatch, newBatch, equipList, params);
+		
+		// 4. 작업 병합 후 박스 요청 
+		retCnt += this.instructBatchBoxReq(mainBatch, equipList, params);
+		
+		return retCnt;
 	}
 
 	@Override
@@ -129,11 +157,13 @@ public class DpsInstructionService extends AbstractInstructionService implements
 	private int instructBatchOrderTypeCallProc(JobBatch batch, List<?> equipList, Object... params) {
 		int resultCnt = 0;
 		
-		// 1. 파라미터 생성
-		Map<String, Object> paramMap = ValueUtil.newMap("P_IN_DOMAIN_ID,P_IN_BATCH_ID", batch.getDomainId(), batch.getId());
-		// 2. 프로시져 콜 
-		Map<?, ?> result = this.queryManager.callReturnProcedure("OP_SET_BATCH_ORDER_TYPE", paramMap, Map.class);
-		// 3. 결과 
+		// 1. 단포 작업 활성화 여부 
+		boolean useSinglePack = DpsBatchJobConfigUtil.isSingleSkuNpcsClassEnabled(batch);
+		// 2. 파라미터 생성
+		Map<String, Object> paramMap = ValueUtil.newMap("P_IN_DOMAIN_ID,P_IN_BATCH_ID,P_IN_SINGLE_PACK", batch.getDomainId(), batch.getId(),useSinglePack);
+		// 3. 프로시져 콜 
+		Map<?, ?> result = this.queryManager.callReturnProcedure("OP_DPS_BATCH_SET_ORDER_TYPE", paramMap, Map.class);
+		// 4. 결과 
 		resultCnt += ValueUtil.toInteger(result.get("P_OUT_MT_COUNT"));
 		resultCnt += ValueUtil.toInteger(result.get("P_OUT_OT_COUNT"));
 		
@@ -159,7 +189,7 @@ public class DpsInstructionService extends AbstractInstructionService implements
 		int resultCnt = 0;
 		
 		// 3. 작업 지시 
-		resultCnt += this.instructBatchStartCallProc(batch, equipList, params);
+		resultCnt += this.instructBatchStartCallProc(batch, params);
 		
 		// 4. 후처리 이벤트 
 		EventResultSet aftResult = this.instructBatchStartEvent(EventConstants.EVENT_STEP_AFTER, batch, equipList, params);
@@ -177,27 +207,23 @@ public class DpsInstructionService extends AbstractInstructionService implements
 	/**
 	 * 작업 지시 프로시저 실행 
 	 * @param batch
-	 * @param equipList
 	 * @param params
 	 * @return
 	 */
-	private int instructBatchStartCallProc(JobBatch batch, List<?> equipList, Object...params) {
-
-		List<String> equipIdList = new ArrayList<String>();
-		for(Object equip : equipList) equipIdList.add(((Rack)equip).getId());
-		
-		// 재고 적치 추천 셀 사용 유무 
-		boolean useRecommendCell = DpsBatchJobConfigUtil.isRecommendCellEnabled(batch);
-		// 호기별 배치 분리 여부
-		boolean useSeparatedBatch = DpsBatchJobConfigUtil.isSeparatedBatchByRack(batch);
+	private int instructBatchStartCallProc(JobBatch batch, Object...params) {
 		// 단포 작업 활성화 여부 
 		boolean useSinglePack = DpsBatchJobConfigUtil.isSingleSkuNpcsClassEnabled(batch);
-		
+		// 재고 적치 추천 셀 사용 유무 
+		boolean useRecommendCell = DpsBatchJobConfigUtil.isRecommendCellEnabled(batch);
+		// 호기별 배치 분리 여부 TODO
+		//boolean useSeparatedBatch = DpsBatchJobConfigUtil.isSeparatedBatchByRack(batch);
+		boolean useSeparatedBatch = false;
+
 		// 1. 파라미터 생성
-		Map<String, Object> paramMap = ValueUtil.newMap("P_IN_DOMAIN_ID,P_IN_BATCH_ID,P_IN_EQUIP_ID_LIST,P_IN_RECOMMEND_CELL,P_IN_SEPARATE_BATCH,P_IN_SINGLE_PACK"
-				, batch.getDomainId(), batch.getId(),ValueUtil.listToString(equipIdList), useRecommendCell, useSeparatedBatch, useSinglePack);
+		Map<String, Object> paramMap = ValueUtil.newMap("P_IN_DOMAIN_ID,P_IN_BATCH_ID,P_IN_SINGLE_PACK,P_IN_RECOMMEND_CELL,P_IN_SEPARATE_BATCH"
+				, batch.getDomainId(), batch.getId(), useSinglePack, useRecommendCell, useSeparatedBatch);
 		// 2. 프로시져 콜 
-		this.queryManager.callReturnProcedure("OP_INSTRUCT_DPS_BATCH", paramMap, Map.class);
+		this.queryManager.callReturnProcedure("OP_DPS_BATCH_INSTRUCT", paramMap, Map.class);
 		
 		return 1;
 	}
@@ -220,6 +246,70 @@ public class DpsInstructionService extends AbstractInstructionService implements
 		}
 		return 0;
 	}
+	
+	
+	
+	/******** 작업 병합 ************/
+	/**
+	 * 작업 지시 
+	 * @param batch
+	 * @param equipList
+	 * @param params
+	 * @return
+	 */
+	private int mergeBatchStart(JobBatch mainBatch, JobBatch newBatch, List<?> equipList, Object... params) {
+		// 1. 전처리 이벤트   
+		EventResultSet befResult = this.mergeBatchStartEvent(EventConstants.EVENT_STEP_BEFORE, mainBatch, newBatch, equipList, params);
+		
+		// 2. 다음 처리 취소 일 경우 결과 리턴 
+		if(befResult.isAfterEventCancel()) {
+			return ValueUtil.toInteger(befResult.getResult());
+		}
+		
+		int resultCnt = 0;
+		
+		// 3. 작업 지시 
+		resultCnt += this.mergeBatchStartCallProc(mainBatch, newBatch, params);
+		
+		// 4. 후처리 이벤트 
+		EventResultSet aftResult = this.mergeBatchStartEvent(EventConstants.EVENT_STEP_AFTER, mainBatch, newBatch, equipList, params);
+		
+		// 5. 후처리 이벤트가 실행 되고 리턴 결과가 있으면 해당 결과 리턴 
+		if(aftResult.isExecuted()) {
+			if(aftResult.getResult() != null ) { 
+				resultCnt += ValueUtil.toInteger(aftResult.getResult());
+			}
+		}
+
+		return resultCnt;
+	}
+	
+	
+
+	/**
+	 * 작업 병합 프로시저 실행 
+	 * @param batch
+	 * @param params
+	 * @return
+	 */
+	private int mergeBatchStartCallProc(JobBatch mainBatch, JobBatch newBatch, Object...params) {
+		// 단포 작업 활성화 여부 
+		boolean useSinglePack = DpsBatchJobConfigUtil.isSingleSkuNpcsClassEnabled(mainBatch);
+		// 재고 적치 추천 셀 사용 유무 
+		boolean useRecommendCell = DpsBatchJobConfigUtil.isRecommendCellEnabled(mainBatch);
+		// 호기별 배치 분리 여부 TODO
+		//boolean useSeparatedBatch = DpsBatchJobConfigUtil.isSeparatedBatchByRack(batch);
+		boolean useSeparatedBatch = false;
+
+		// 1. 파라미터 생성 TODO
+//		Map<String, Object> paramMap = ValueUtil.newMap("P_IN_DOMAIN_ID,P_IN_BATCH_ID,P_IN_SINGLE_PACK,P_IN_RECOMMEND_CELL,P_IN_SEPARATE_BATCH"
+//				, batch.getDomainId(), batch.getId(), useSinglePack, useRecommendCell, useSeparatedBatch);
+		// 2. 프로시져 콜 
+//		this.queryManager.callReturnProcedure("OP_DPS_BATCH_MERGE", paramMap, Map.class);
+		
+		return 1;
+	}
+	
 	
 	
 	/** 전 / 후 처리 이벤트 처리  **/
